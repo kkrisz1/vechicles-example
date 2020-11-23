1665bad1cab18c6d8dc395800b5e79c87b5e5faf
kkrisz1 <kukrisz@gmail.com>
2020-11-23 20:42:03 +0100

    create the basic application

---
+++ vehicles-web/src/main/java/org/example/vehicles/web/VehiclesApplication.java
@@ -0,0 +1,11 @@
+package org.example.vehicles.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VehiclesApplication.class, args);
+    }
+}
