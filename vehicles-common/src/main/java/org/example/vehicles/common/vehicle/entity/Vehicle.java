7f6d83df369f5a9a9312bae092fc6178f95e66ab
kkrisz1 <kukrisz@gmail.com>
2020-11-27 10:40:04 +0100

    [refactor] JSON ser: include only non-null vehicle properties

--- vehicles-common/src/main/java/org/example/vehicles/common/vehicle/entity/Vehicle.java
+++ vehicles-common/src/main/java/org/example/vehicles/common/vehicle/entity/Vehicle.java
@@ -1,5 +1,6 @@
 package org.example.vehicles.common.vehicle.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Vehicle {
     private UUID id;
     private Location location;
